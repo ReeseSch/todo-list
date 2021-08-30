d9f21d43a124f280d1370bdaea2e164bc2e25549
Reese Schumacher <reesesch1@gmail.com>
2021-08-30 13:58:31 -0600

    first commit

---
+++ src/redux/todoSlice.js
@@ -0,0 +1,70 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+// import { useDispatch } from "react-redux";
+
+export const getTodosAsync = createAsyncThunk('todos/getTodosAsync',
+async() => {
+    const response = await fetch('http://localhost:7000/todos')
+    if(response.ok) {
+        const todos = await response.json()
+        return { todos }
+    }
+})
+
+export const addTodoAsync = createAsyncThunk('todos/addTodoAsync', async(payload) => {
+    const response = await fetch('http://localhost:7000/todos', {
+        method: "POST",
+        headers: {
+            'Content-Type': "application/json",
+        },
+        body: JSON.stringify({title: payload.title})
+    })
+
+    if(response.ok){
+        const todo = await response.json()
+        return { todo }
+    }
+})
+
+const todoSlice = createSlice({
+    name: "todos",
+    initialState: [
+        {id: 1, title: "todo1", completed: false},
+        {id: 2, title: "todo2", completed: false},
+        {id: 3, title: "todo3", completed: false}
+    ],
+    reducers: {
+        addTodo: (state, action) => {
+            const newTodo = {
+                id: Date.now(),
+                title: action.payload.title,
+                completed: false
+        }
+        state.push(newTodo)
+    },
+    toggleComplete: (state, action) => {
+        const index = state.findIndex((todo)=> todo.id === action.payload.id)
+        state[index].completed = action.payload.completed
+    },
+    deleteTodo: (state, action) => {
+        return state.filter((todo)=> todo.id !== action.payload.id )
+    }
+},
+    extraReducers: {
+        [getTodosAsync.pending]: (state, action) => {
+            console.log('fetching data...')
+        },
+        [getTodosAsync.fulfilled]: (state, action) => {
+            console.log(`We've got the data!`)
+            return action.payload.todos
+        },
+        [addTodoAsync.fulfilled]: (state, action) => {
+            state.push(action.payload.todo)
+        },
+    }
+
+});
+
+export const { addTodo, toggleComplete, deleteTodo } = todoSlice.actions
+
+
+export default todoSlice.reducer
